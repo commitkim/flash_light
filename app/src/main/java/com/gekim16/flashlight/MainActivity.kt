ba164f766989c68ab1d3fcdcbf18aff61a87b523
Ken GW <gekim16@coupang.com>
2021-01-25 00:41:11 +0900

    Add other way of flickering

--- app/src/main/java/com/gekim16/flashlight/MainActivity.kt
+++ app/src/main/java/com/gekim16/flashlight/MainActivity.kt
@@ -1,26 +1,56 @@
 package com.gekim16.flashlight
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
-import android.content.Context
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 private const val ON = "on"
 private const val OFF = "off"
 
 class MainActivity : AppCompatActivity() {
     private var state = OFF
+    private var isFlickering = false
 
-    private lateinit var cameraManager : CameraManager
+    private lateinit var cameraManager: CameraManager
     private var cameraId: String? = null
-    private var thread : Thread? = null
-    private val seekBarMax : Int by lazy { seekBar.max }
+    private val seekBarMax: Int by lazy { seekBar.max }
+
+    private var thread: Thread? = null
+
+
+    private val handler: Handler = Handler(Looper.getMainLooper())
+    private val intervalTask: Runnable by lazy { object : Runnable {
+        override fun run() {
+            if (!isFlickering) return
+
+            flicker()
+            handler.postDelayed(this, getInterval())
+        }
+    } }
+
+    private var timer: Timer? = null
+
+    private var job : Job? = null
+
+    private var observable : Disposable? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,37 +59,38 @@ class MainActivity : AppCompatActivity() {
         setNoPermissionFlashListener()
     }
 
-    private fun setNoPermissionFlashListener(){
+    private fun setNoPermissionFlashListener() {
         initFlashlight()
         flashButton.setOnClickListener {
-            when(state){
-                ON ->{
-                    blinkFlash(-1)
-                }
-                OFF ->{
-                    blinkFlash((seekBarMax - seekBar.progress))
-                }
+
+            if(isFlickering) {
+                stopFlickering()
             }
+            else{
+                startFlickeringWithRxJava()
+            }
+            isFlickering = !isFlickering
         }
 
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
             }
+
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                if(state == ON)
-                    blinkFlash(seekBarMax)
+
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar == null) return
-                if(state == ON)
-                    blinkFlash((seekBarMax - seekBar.progress))
+                seekBar?.let{
+                    if(isFlickering)
+                        startFlickeringWithRxJava()
+                }
             }
         })
     }
 
-    private fun initFlashlight(){
-        if(!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+    private fun initFlashlight() {
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
             Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show()
             return
         }
@@ -70,56 +101,64 @@ class MainActivity : AppCompatActivity() {
             val flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)
             val lensFacing = c.get(CameraCharacteristics.LENS_FACING)
 
-            if(flashAvailable != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK){
+            if (flashAvailable != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
                 cameraId = it
             }
         }
     }
 
-    private fun flashLightOn(){
+    private fun getInterval() = ((seekBarMax - seekBar.progress) * 10).toLong()
+
+    private fun flashLightOn() {
         cameraId?.let {
             state = ON
-            cameraManager.setTorchMode(it,true)
+            cameraManager.setTorchMode(it, true)
         }
     }
 
-    private fun flashLightOff(){
+    private fun flashLightOff() {
         cameraId?.let {
             state = OFF
-            cameraManager.setTorchMode(it,false)
+            cameraManager.setTorchMode(it, false)
         }
     }
 
-    private fun blinkFlash(frequency: Int) {
-        thread?.interrupt()
-        thread = null
+    private fun flicker() {
+        if (state == OFF) {
+            flashLightOn()
+        } else {
+            flashLightOff()
+        }
+    }
 
-        if(frequency == seekBarMax) {
+    // isContinue 등의 변수로 종료시켰을 때 확실히 꺼지지 않으면 두개의 깜빡이는 메소드가 동작되는 문제 발생으로 확실히 종료시켜주는 메소드를 추가하는 방법 선택
+    private fun stopFlickering(){
+        thread?.interrupt()
+        job?.cancel()
+        timer?.cancel()
+        observable?.dispose()
+
+        flashLightOff()
+    }
+
+    /**
+     * Use Thread
+     */
+    private fun startFlickeringWithThread() {
+        stopFlickering()
+
+        if (seekBar.progress == 0) {
             flashLightOn()
             return
         }
-        if(frequency < 0) {
-            flashLightOff()
-            return
-        }
 
-        val delay = (frequency * 10).toLong()
         thread = Thread(Runnable {
             try {
-                var mode = OFF
-
-                while(true){
-                    mode = if(mode == OFF){
-                        flashLightOn()
-                        ON
-                    } else{
-                        flashLightOff()
-                        OFF
-                    }
-
-                    Thread.sleep(delay)
+                while(isFlickering){
+                    flicker()
+                    Thread.sleep(getInterval())
                 }
-            }catch (e : Exception){
+            } catch (e: Exception) {
                 e.printStackTrace()
             }
         })
@@ -127,4 +166,78 @@ class MainActivity : AppCompatActivity() {
         thread?.start()
     }
 
+
+    /**
+     *  Use Handler
+     */
+    private fun startFlickeringWithHandler() {
+        if (seekBar.progress == 0) {
+            flashLightOn()
+            return
+        }
+
+        handler.post(intervalTask)
+    }
+
+
+    /**
+     *  Use Coroutine
+     */
+    private fun startFlickeringWithCoroutine(){
+        stopFlickering()
+
+        if (seekBar.progress == 0) {
+            flashLightOn()
+            return
+        }
+
+        job = CoroutineScope(Default).launch{
+            while(isFlickering){
+                flicker()
+                delay(getInterval())
+            }
+        }
+    }
+
+    /**
+     *  Use Timer
+     */
+    private fun startFlickeringWithTimer(){
+        stopFlickering()
+
+        if (seekBar.progress == 0) {
+            flashLightOn()
+            return
+        }
+
+        val interval = getInterval()
+        timer = Timer()
+
+        timer?.schedule(object : TimerTask() {
+            override fun run() {
+                flicker()
+            }
+        }, 0, if(interval <= 0 ) 1 else interval) // 0 이하의 딜레이일땐 에러가 발생
+    }
+
+    /**
+     *  Use RxJava
+     */
+    private fun startFlickeringWithRxJava(){
+        stopFlickering()
+
+        if (seekBar.progress == 0) {
+            flashLightOn()
+            return
+        }
+
+        observable = io.reactivex.rxjava3.core.Observable.timer(getInterval(), TimeUnit.MILLISECONDS)  // 몇 ms 뒤에 실핼할 것인지 결정
+            .repeat() //반복
+            .subscribeOn(Schedulers.computation())  // subscribe 에 사용할 쓰레드 지정
+            .observeOn(Schedulers.newThread()) // Observable 이 다음 처리를 진행할 때 사용할 쓰레드를 지정
+            .subscribe {
+                if(isFlickering)
+                    flicker()
+            }
+    }
 }
